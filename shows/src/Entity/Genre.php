84500d4b3ae4d078cac2a9c48c1f9d8ba2b811ae
Bonnot Clement <clement.bonnot@etu.u-bordeaux.fr>
2022-01-06 16:26:04 +0100

    Connection BD

---
+++ shows/src/Entity/Genre.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Genre
+ *
+ * @ORM\Table(name="genre")
+ * @ORM\Entity
+ */
+class Genre
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=128, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\ManyToMany(targetEntity="Series", inversedBy="genre")
+     * @ORM\JoinTable(name="genre_series",
+     *   joinColumns={
+     *     @ORM\JoinColumn(name="genre_id", referencedColumnName="id")
+     *   },
+     *   inverseJoinColumns={
+     *     @ORM\JoinColumn(name="series_id", referencedColumnName="id")
+     *   }
+     * )
+     */
+    private $series;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->series = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+}
