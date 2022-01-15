f08106cab4a22ab8310472393d75efd10b238bc6
tpaolini <timothe.paolini@etu-u.bordeaux.fr>
2022-01-15 09:40:01 +0100

    test branche

--- shows/src/Controller/SeriesController.php
+++ shows/src/Controller/SeriesController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/series')]
 class SeriesController extends AbstractController
 {
-    #[Route('/', name: 'series_index', methods: ['GET'])]
+    /*#[Route('/', name: 'series_index', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager): Response
     {
         $series = $entityManager
@@ -25,7 +25,67 @@ class SeriesController extends AbstractController
         return $this->render('series/index.html.twig', [
             'series' => $series,
         ]);
+    }*/
+
+    #[Route('/', name: 'series_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $titre = $_GET['titre'] ?? "";
+        $ADebut = $_GET['An_Debut'] ?? 1899-03-21;
+        $AFin = $_GET['An_Fin'] ?? 2023-01-01;
+        $page = $_GET['Page'] ?? 0;
+        //$triNote = $_Get['triNote'] ?? "Pas coché";
+
+        if($page <= 0 ) { $page = 1;}
+        
+        $page = $page-1;
+        if(isset($_GET['triNote'])){
+            $em = $this->getDoctrine()->getManager();
+            $repository = $em->getRepository(Rating::class);
+            $series = $repository->createQueryBuilder('r')
+                //->select('s')
+                //->distinct()
+                //->from(Rating::class,'r')
+                ->innerJoin('r.series', 's','WITH', 's.id = r.series')
+                ->where ('s.title like ?1 AND s.yearStart >= ?2 AND 
+                   (s.yearEnd <= ?3 OR s.yearEnd IS NULL)')
+                ->orderBy('AVG(r.value)','ASC'/*,'s.title','ASC'*/)
+                ->setParameter(1,'%'.$titre.'%')
+                ->setParameter(2,$ADebut)
+                ->setParameter(3,$AFin)
+                ->setFirstResult($page*10)
+                ->setMaxResults(9)
+                ->getQuery()
+                ->getResult()
+                ;
+        } else {
+            $em = $this->getDoctrine()->getManager();
+            $repository = $em->getRepository(Series::class);
+            $series = $repository->createQueryBuilder('s')
+                ->where ('s.title like ?1 AND s.yearStart >= ?2 AND 
+                   (s.yearEnd <= ?3 OR s.yearEnd IS NULL)')
+                ->orderBy('s.title','ASC')
+                ->setParameter(1,'%'.$titre.'%')
+                ->setParameter(2,$ADebut)
+                ->setParameter(3,$AFin)
+                ->setFirstResult($page*10)
+                ->setMaxResults(9)
+                ->getQuery()
+                ->getResult()
+                ;
+        }
+        
+        $page = $page+1;
+
+        return $this->render('series/index.html.twig', [
+            'series' => $series, 
+            'titre'=> $titre, 
+            'ADebut' => $ADebut,
+            'AFin' => $AFin,
+            'page' => $page
+        ]);
     }
+
 
     #[Route('/new', name: 'series_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
@@ -47,15 +107,60 @@ class SeriesController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'series_show', methods: ['GET'])]
+    /*#[Route('/{id}', name: 'series_show', methods: ['GET'])]
     public function show(Series $series): Response
     {
         return $this->render('series/show.html.twig', [
             'series' => $series,
         ]);
+    }*/
+    #[Route('/{id}', name: 'series_show', methods: ['GET'])]
+    public function show(Series $series): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repositorySai = $em->getRepository(Season::class);
+
+        $saison = $repositorySai->createQueryBuilder('s')
+            ->where ('s.series = ?1')
+            ->orderBy('s.number', 'ASC')
+            ->setParameter(1,$series->getId())
+            ->getQuery()
+            ->getResult()
+            ;
+        $repositoryEp = $em->getRepository(Episode::class);
+        $episode = $repositoryEp->createQueryBuilder('e')
+            ->innerJoin('e.season', 's','WITH', 'e.season = s.id')
+            ->where('s.series = ?1')
+            ->orderBy('e.number','ASC')
+            ->setParameter(1,$series->getId())
+            ->getQuery()
+            ->getResult()
+            ;
+        return $this->render('series/show.html.twig', [
+            'series' => $series, 'seasons' => $saison, 'episodes' => $episode
+        ]);
     }
 
-    #[Route('/{id}/edit', name: 'series_edit', methods: ['GET', 'POST'])]
+    #[Route('/poster/{id}', name: 'series_poster_show', methods: ['GET'])]
+    public function showPoster(Series $series): Response
+    {
+        return new Response (stream_get_contents($series->getPoster()),
+            200, array('Content-type' => 'image/jpeg',
+        ));
+    }
+
+    #[Route('/trailer/{id}', name: 'series_trailer_show', methods: ['GET'])]
+    public function showTrailer(Series $series) : Response
+    {
+        $lien_trailer = $series->getYoutubeTrailer();
+        $nouv_trailer = substr($lien_trailer,0,24);
+        $nouv_trailer = $nouv_trailer."embed";
+        $nouv_trailer = $nouv_trailer.substr($lien_trailer,29);
+        //$lien_tailer = preg_replace("watch","embed",$lien_trailer);
+        return new Response ($nouv_trailer);
+    }
+    
+    /*#[Route('/{id}/edit', name: 'series_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Series $series, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(SeriesType::class, $series);
@@ -90,5 +195,5 @@ class SeriesController extends AbstractController
         return new Response (stream_get_contents($series->getPoster()),
             200, array('Content-type' => 'image/jpeg',
         ));
-    }
+    }*/
 }
